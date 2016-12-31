423f22906f6fb0b4b55a189b5fe7c168b3551326
Takuma Kuramitsu <kuramitsu.takuma@gmail.com>
2017-01-01 00:03:39 +0900

    first commit

---
+++ src/FlexApp.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="app">
+    <header>
+      <div>FlexVue</div>
+    </header>
+    <main>
+      <flex></flex>
+    </main>
+    <footer>
+      <div>&copy; kuramitsu</div>
+    </footer>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+import Flex from './components/Flex';
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    Flex
+  }
+}
+</script>
+
+<style>
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+#app {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+header {
+  width: 100%;
+  height: 40px;
+  background-color:#444;
+  color: #CCC;
+  flex-shrink: 0;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+}
+header>div {
+  margin: 10px;
+}
+main {
+  height: calc(100% - 70px);
+  width: 100%;
+  overflow: auto;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+}
+footer {
+  width: 100%;
+  height: 30px;
+  background-color:#444;
+  color: #CCC;
+  flex-shrink: 0;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+}
+
+
+
+
+/*
+header {
+  position: fixed;
+  padding: 0px 10px;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 40px;
+  background-color:#444;
+  color: #CCC;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: flex-start;
+  align-items: center;
+}
+main {
+  overflow: auto;
+  margin: 0;
+  padding-top: 40px;
+  padding-bottom: 30px;
+  height: calc(100% - 70px);
+  width: 100%;
+  min-height: 200px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+}
+footer {
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  height: 30px;
+  background-color:#444;
+  color: #CCC;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+}
+*/
+</style>
---
+++ src/components/Flex.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="dashboard">
+    <div class="item1"></div>
+    <div class="item2"></div>
+    <div class="item3"></div>
+    <div class="item4"></div>
+    <div class="item1"></div>
+    <div class="item2"></div>
+    <div class="item3"></div>
+    <div class="item4"></div>
+    <div style="
+      background-color: #325496;
+      flex-grow: 10;
+      width: 100px;
+      min-height: 100px;
+    "></div>
+    <div style="
+      background-color: #5d729a;
+      flex-grow: 5;
+      width: 100px;
+      min-height: 200px;
+    "></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.dashboard {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  align-items: stretch;
+  width: 100%;
+  height: 100%;
+}
+.item1 {
+  background-color: #325496;
+  flex-grow: 1;
+  width: 100px;
+  min-height: 100px;
+}
+.item2 {
+  background-color: #5a92e7;
+  flex-grow: 2;
+  width: 200px;
+  min-height: 100px;
+}
+.item3 {
+  background-color: #238ade;
+  flex-grow: 3;
+  width: 100px;
+  min-height: 100px;
+}
+.item4 {
+  background-color: #14687e;
+  flex-grow: 4;
+  width: 200px;
+  min-height: 100px;
+}
+
+
+</style>
